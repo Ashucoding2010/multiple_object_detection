1436b271c0fae0c9110097fd41316f757388995b
Ashucoding2010 <70793998+Ashucoding2010@users.noreply.github.com>
2021-08-29 13:09:14 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,79 @@
+status=""
+objects=[]//since answers are going to be more than one so we are using array
+
+
+img = ""
+function preload(){
+
+img = loadImage("img1.jpg")
+
+}
+
+function setup(){
+
+Canvas = createCanvas(580,450)
+Canvas.position(450,190)
+//COCOSSD Model starts here
+objectDetector=ml5.objectDetector('cocossd',modelloaded)
+document.getElementById("status").innerHTML= "Model Loaded !!!"
+}
+
+function modelloaded() {
+console.log("Model Loaded Sucessfully")
+status=true
+objectDetector.detect(img,gotResult)
+
+}
+
+function gotResult(error,results){
+
+if(error){
+
+    console.log(error)
+}
+else{
+
+console.log(results)
+objects = results
+}
+}
+
+function draw(){
+
+    image(img,0,0,580,450)
+    if(status != ""){
+
+for(i=0;i<objects.length;i++){
+
+    document.getElementById("status").innerHTML="status:Object detected"
+    obj_name=objects[i].label
+    per=objects[i].confidence
+    perR=floor(per*100)
+    height=objects[i].height
+    width=objects[i].width
+    pose_x=objects[i].x
+    pose_y=objects[i].y
+    fill("red")
+    textSize(20)
+    stroke("yellow")
+    text(obj_name+" "+perR+"%",pose_x+20,pose_y+20)
+    strokeWeight(4)
+    noFill()
+    rect(pose_x,pose_y,width,height)
+}
+
+    }
+    }
+
+    function home(){
+
+    window.location="index.html"
+
+    }
+    
+
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,23 @@
+function f1() {
+
+    window.location = "first.html"
+
+}
+
+function f2() {
+
+    window.location = "second.html"
+
+}
+
+function f3() {
+
+    window.location = "third.html"
+
+}
+
+function f4() {
+
+    window.location = "fourth.html"
+
+}
